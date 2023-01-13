9fd862af0008e55b13f7520649e1ffa29a3b2941
HAZZABROI <justadollrussia@gmail.com>
2023-01-14 00:16:41 +0300

    add reset activity

--- app/src/main/java/io/skfx/skfxschoolmobile/LoginActivity.java
+++ app/src/main/java/io/skfx/skfxschoolmobile/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Scanner;
 
@@ -18,11 +19,17 @@ public class LoginActivity extends AppCompatActivity {
         Scanner sc = null;
         System.out.println(sc);
         Button btn = (Button)findViewById(R.id.button2);
-
+        TextView forgetb = (TextView)findViewById(R.id.textView3);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+        forgetb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResetActivity.class));
             }
         });
 
---
+++ app/src/main/java/io/skfx/skfxschoolmobile/ResetActivity.java
@@ -0,0 +1,26 @@
+package io.skfx.skfxschoolmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ResetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset);
+
+        View view = (View)findViewById(R.id.backb);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResetActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
