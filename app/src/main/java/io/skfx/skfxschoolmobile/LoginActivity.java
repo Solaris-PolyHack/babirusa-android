424f6406cb0d8091389fa9413c6eaf160ebc6749
antoxa421 <justadollrussia@gmail.com>
2023-01-13 20:03:54 +0300

    add login login activity

--- app/src/main/java/io/skfx/skfxschoolmobile/LoginActivity.java
+++ app/src/main/java/io/skfx/skfxschoolmobile/LoginActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 import java.util.Scanner;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Scanner sc = null;
         System.out.println(sc);
     }
